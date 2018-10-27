c4d6c2976fb34fb3683c0d3a53b15d69148e3cd0
kraxarn <emil202821@hotmail.com>
2018-10-27 23:01:20 +0200

    Initial CookieWebClient class

---
+++ ProjectFlightApp/ProjectFlightApp/CookieWebClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+
+namespace ProjectFlightApp
+{
+	/// <summary>
+	/// Extension of <see cref="WebClient"/> which adds support for cookie storage
+	/// </summary>
+	public class CookieWebClient : WebClient
+	{
+		// Public way to access cookies
+		public CookieContainer CookieContainer { get; }
+
+		// Add to constructor and create the cookie container
+		public CookieWebClient() => CookieContainer = new CookieContainer();
+
+		// Override GetWebRequest to steal the cookies
+		protected override WebRequest GetWebRequest(Uri address)
+		{
+			var request = base.GetWebRequest(address) as HttpWebRequest;
+
+			if (request != null)
+				request.CookieContainer = CookieContainer;
+
+			return request;
+		}
+	}
+}

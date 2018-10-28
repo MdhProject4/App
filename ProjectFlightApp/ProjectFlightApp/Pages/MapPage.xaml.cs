df3bfed76038c78b74768ed8680e789174ef6f53
kraxarn <emil202821@hotmail.com>
2018-10-28 17:14:26 +0100

    Hide the default webpage ui

--- ProjectFlightApp/ProjectFlightApp/Pages/MapPage.xaml.cs
+++ ProjectFlightApp/ProjectFlightApp/Pages/MapPage.xaml.cs
@@ -5,9 +5,12 @@ namespace ProjectFlightApp.Pages
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MapPage
 	{
-		public MapPage ()
+		public MapPage()
 		{
 			InitializeComponent();
+
+			// Hide the default UI once the page is loaded
+			WebViewMap.Navigated += async (sender, args) => { await WebViewMap.EvaluateJavaScriptAsync("hideUi()"); };
 		}
 	}
 }

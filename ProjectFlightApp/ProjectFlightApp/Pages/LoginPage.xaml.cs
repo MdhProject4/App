1ff3cbd57224d0440bfd4438dc92555219469da0
kraxarn <emil202821@hotmail.com>
2018-10-28 17:14:08 +0100

    Remove unused code

--- ProjectFlightApp/ProjectFlightApp/Pages/LoginPage.xaml.cs
+++ ProjectFlightApp/ProjectFlightApp/Pages/LoginPage.xaml.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms.Xaml;
 
 namespace ProjectFlightApp.Pages
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LoginPage
 	{
-		public LoginPage ()
-		{
-			InitializeComponent ();
-		}
-
-		private async void CancelButton_OnClicked(object sender, EventArgs e)
-		{
-		}
+		public LoginPage() => InitializeComponent();
 	}
 }

c86887ed4f45519a3daeb3af1766edb2a335baf8
kraxarn <emil202821@hotmail.com>
2018-10-29 17:39:34 +0100

    Add notification class

---
+++ ProjectFlightApp/ProjectFlightApp/Notification.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectFlightApp
+{
+	public class Notification
+	{
+		public string Id { get; set; }
+		
+		public string Username { get; set; }
+
+		public string FlightId { get; set; }
+
+		public bool Notified;
+	}
+}

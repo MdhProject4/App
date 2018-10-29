e389eb62dc06887cb1c6fe2be324987d5b3dad76
kraxarn <emil202821@hotmail.com>
2018-10-29 17:38:21 +0100

    Show notifications

--- ProjectFlightApp/ProjectFlightApp/Pages/AccountPage.xaml.cs
+++ ProjectFlightApp/ProjectFlightApp/Pages/AccountPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,10 +9,12 @@ namespace ProjectFlightApp.Pages
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class AccountPage
 	{
-		public AccountPage()
-		{
-			InitializeComponent();
-		}
+		public AccountPage() => InitializeComponent();
+
+		/// <summary>
+		/// Notifications shown in the notifications view
+		/// </summary>
+		private ObservableCollection<Notification> notifications;
 
 		protected override void OnAppearing()
 		{
@@ -18,13 +22,20 @@ namespace ProjectFlightApp.Pages
 
 			// Set values on page
 			LabelUsername.Text = $"Welcome {Account.Username}!";
+
+			// Update notifications list
+			var notificationsList = JsonConvert.DeserializeObject<Notification[]>(Account.Request("user/getNotifications"));
+
+			// Set item sources
+			notifications = new ObservableCollection<Notification>(notificationsList);
+			ViewNotifications.ItemsSource = notifications;
 		}
 
 		private async void ButtonTestLogin_OnClicked(object sender, EventArgs e)
 		{
-			var response = Account.Request("user");
+			var response = JsonConvert.DeserializeObject<JsonUserResult>(Account.Request("user"));
 
-			await DisplayAlert("Login", response, "Dismiss");
+			await DisplayAlert("Login", $"{response}", "Dismiss");
 		}
 
 		private async void ButtonSwitchAccount_OnClicked(object sender, EventArgs e) => 
